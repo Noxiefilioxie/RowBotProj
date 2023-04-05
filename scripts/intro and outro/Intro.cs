77529f642365905a08a63a7bea41220122c6e170
Jonken <Noxxan@protonmail.com>
2023-04-05 19:30:02 +0200

    Added animated text

--- scripts/intro and outro/Intro.cs
+++ scripts/intro and outro/Intro.cs
@@ -3,28 +3,76 @@ using System;
 
 public partial class Intro : Node2D
 {
+	public delegate AnimationPlayer AnimationPlayerDelegate();
 
 	[Export]
 	public AnimatedSprite2D Sprite;
 	[Export]
 	public PackedScene MainScene;
+	[Export]
+	public AnimationPlayer AnimationPlayer;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 
+		Sprite.Play("1");
+		AnimationPlayer.Play("Text1");
+
+		AnimationPlayer.AnimationFinished += OnAnimationFinished;
+
 
 
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
 	{
+
 		if (!Sprite.IsPlaying())
 		{
 			GetTree().ChangeSceneToFile(MainScene.ResourcePath);
 
 		}
 	}
+
+
+    private void OnAnimationFinished(StringName animName)
+    {
+        GD.Print("ANIMATION FINISHED: " + animName);
+		if (animName == "Text1")
+		{
+			Sprite.Play("2");
+			AnimationPlayer.Play("Text2");
+		}
+		else if (animName == "Text2")
+		{
+			Sprite.Play("3");
+			AnimationPlayer.Play("Text3");
+		}
+		else if (animName == "Text3")
+		{
+			Sprite.Play("4");
+			AnimationPlayer.Play("Text4");
+		}
+		else if (animName == "Text4")
+		{
+			Sprite.Play("5");
+			AnimationPlayer.Play("Text5");
+		}
+		else if (animName == "Text5")
+		{
+			Sprite.Play("6");
+			AnimationPlayer.Play("Text6");
+		}
+		else if (animName == "Text6")
+		{			
+			GetTree().ChangeSceneToFile(MainScene.ResourcePath);
+		}
+
+    }
+
+
 
 }
