df098abce4261b3c9cc1876da114ff325ff98af0
rochtenfeldt <rickard.hammarberg94@gmail.com>
2023-04-05 18:46:37 +0200

    merge fix

--- scripts/intro and outro/Intro.cs
+++ scripts/intro and outro/Intro.cs
@@ -5,7 +5,6 @@ using System.Security.Cryptography.X509Certificates;
 public partial class Intro : Node2D
 {
 
-<<<<<<< HEAD
     [Export]
     public AnimatedSprite2D Sprite;
     [Export]
@@ -47,34 +46,4 @@ public partial class Intro : Node2D
 
       
     }
-
-    public void _on_animated_sprite_2d_animation_finished()
-    {
-        GD.Print("FInished");
-    }
-=======
-	[Export]
-	public AnimatedSprite2D Sprite;
-	[Export]
-	public PackedScene MainScene;
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-
-		Sprite.Play("1");
-
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		if (!Sprite.IsPlaying())
-		{
-			GetTree().ChangeSceneToFile(MainScene.ResourcePath);
-
-		}
-	}
->>>>>>> c090c3ca96da509ed87af5dfc356166029250bae
-
 }
